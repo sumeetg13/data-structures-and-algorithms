d38026339d8b67f8b957924611478be14b63d380
Sumeet Gupta <sumeet13gupta@gmail.com>
2026-04-08 16:27:16 +0530

    Leetcode 1143. Longest Common Subsequence Solution Added

--- Leetcode/LongestCommonSubsequence/LongestCommonSubsequence.java
+++ Leetcode/LongestCommonSubsequence/LongestCommonSubsequence.java
@@ -10,7 +10,6 @@ public class LongestCommonSubsequence {
             for (int j = 1; j <= n; j++) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                    System.out.println(text1.charAt(i - 1) + "  ");
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
